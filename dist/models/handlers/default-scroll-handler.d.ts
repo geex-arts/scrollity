b5a36fbbbf216a5e62779a074a09ce7c2d02700c
Denis K <f1nal@cgaming.org>
2017-10-13 15:28:16 +0300

    Build

--- dist/models/handlers/default-scroll-handler.d.ts
+++ dist/models/handlers/default-scroll-handler.d.ts
@@ -2,6 +2,6 @@ import { Observable } from 'rxjs/Observable';
 import { ScrollHandler } from './scroll-handler';
 export declare class DefaultScrollHandler extends ScrollHandler {
     getInstantPosition(): number;
-    handleScrollEvent(deltaX: any, deltaY: any, duration: any): void;
+    handleScrollEvent(deltaX: any, deltaY: any, duration: any, ease?: any): void;
     scrollTo(position: any, duration: any, ease?: any, cancellable?: boolean): Observable<{}>;
 }
